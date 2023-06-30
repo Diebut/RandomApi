8992afe04616ec92672aad910cbe5145bc07d8e1
Diebut <guevaradiego_2011@hotmail.com>
2023-06-30 14:55:49 -0300

    se edita la ruta y mandamos por consola los parametros creados de numero inicial y final

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ app.get('/', () =>{
     res.send('index pages')  
 });
 
-app.get('/random/:numeroInicial/:numeroFinal')
+app.get('/random/:numeroInicial/:numeroFinal', (req, res) =>{
+    console.log(req.params.numeroFinal);
+    console.log(req.params.numeroFinal);
+    res.send('recibido');
+})
 
 app.listen(3000, () =>{
     console.log('server on post 3000')
