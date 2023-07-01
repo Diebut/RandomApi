b352457a24d28afaf1e60cef180eeb912d962e87
Diebut <guevaradiego_2011@hotmail.com>
2023-06-30 22:24:48 -0300

    agreamos un if para indetificar si no hay numero que no devuelva un error de peticion 404 en la pagina

--- app.js
+++ app.js
@@ -9,11 +9,17 @@ app.get('/random/:numeroInicial/:numeroFinal', (req, res) =>{
     const min = parseInt(req.params.numeroInicial);
     const max = parseInt(req.params.numeroFinal);
 
+    if (isNaN(min) || isNaN(max)) {
+        res.status(404);
+        res.json({"error": 'Bad requeste'}); //peticion erronea
+        return;
+    }
+
     const result = Math.floor(Math.random() * (max - min) + min)
     //console.log(result);
     
     res.json({"randomNumber": result});
-})
+});
 
 app.listen(3000, () =>{
     console.log('server on post 3000')
